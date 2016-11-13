d5105657f924d56e3a9baf7b0ed457dbc37fb681
pshirshov <pshirshov@gmail.com>
2016-11-13 18:26:29 +0000

    visibility fixes:

--- lib/rest-api/src/main/scala/org/bitbucket/pshirshov/izumitk/http/auth/JwtAuthorizations.scala
+++ lib/rest-api/src/main/scala/org/bitbucket/pshirshov/izumitk/http/auth/JwtAuthorizations.scala
@@ -59,7 +59,7 @@ class JwtAuthorizations @Inject() (
     }
   }
 
-  protected[izumitk] def extractJwtToken(): HttpHeader => Option[String] = {
+  protected def extractJwtToken(): HttpHeader => Option[String] = {
     case Authorization(c: OAuth2BearerToken) =>
       Some(c.token)
 
--- lib/rest-api/src/main/scala/org/bitbucket/pshirshov/izumitk/http/auth/KeyAuthorizations.scala
+++ lib/rest-api/src/main/scala/org/bitbucket/pshirshov/izumitk/http/auth/KeyAuthorizations.scala
@@ -8,7 +8,7 @@ import akka.http.scaladsl.model.headers.{Authorization, GenericHttpCredentials}
 
 
 trait KeyAuthorizations {
-  protected[izumitk] def extractKey(scheme: String): HttpHeader => Option[String] = {
+  protected def extractKey(scheme: String): HttpHeader => Option[String] = {
     case Authorization(c: GenericHttpCredentials) if c.scheme.toLowerCase == scheme.toLowerCase() =>
       c.params.get("")
 
