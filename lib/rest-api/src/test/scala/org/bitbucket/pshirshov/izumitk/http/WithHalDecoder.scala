c6243981f246e0a8024ff45408c1d8bbaf313d7f
Pavel Shirshov <pavel.shirshov@teckro.com>
2016-11-22 13:31:36 +0000

    minor hal fixes

--- lib/rest-api/src/test/scala/org/bitbucket/pshirshov/izumitk/http/WithHalDecoder.scala
+++ lib/rest-api/src/test/scala/org/bitbucket/pshirshov/izumitk/http/WithHalDecoder.scala
@@ -3,7 +3,7 @@ package org.bitbucket.pshirshov.izumitk.http
 import akka.http.scaladsl.testkit.RouteTest
 import akka.http.scaladsl.unmarshalling._
 import com.fasterxml.jackson.databind.node.ObjectNode
-import org.bitbucket.pshirshov.izumitk.http.hal.UnreliableHalDecoder
+import org.bitbucket.pshirshov.izumitk.http.hal.HalDecoder
 import org.bitbucket.pshirshov.izumitk.test.ExposedTestScope
 
 /**
@@ -14,7 +14,7 @@ trait WithHalDecoder {
 
   import scala.reflect.runtime.universe._
 
-  def halResponseAs[T: TypeTag : Manifest](implicit unm: FromResponseUnmarshaller[ObjectNode], decoder: UnreliableHalDecoder): T = {
+  def halResponseAs[T: TypeTag : Manifest](implicit unm: FromResponseUnmarshaller[ObjectNode], decoder: HalDecoder): T = {
     decoder.readHal[T](responseAs[ObjectNode])
   }
 }
