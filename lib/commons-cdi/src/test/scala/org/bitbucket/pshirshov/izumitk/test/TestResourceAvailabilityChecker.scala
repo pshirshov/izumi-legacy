11027bae93615d19c664886710f63764d04e675c
Pavel Shirshov <pavel.shirshov@teckro.com>
2016-12-01 10:47:17 +0000

    net service availability checker

--- lib/commons-cdi/src/test/scala/org/bitbucket/pshirshov/izumitk/test/TestResourceAvailabilityChecker.scala
+++ lib/commons-cdi/src/test/scala/org/bitbucket/pshirshov/izumitk/test/TestResourceAvailabilityChecker.scala
@@ -1,15 +1,48 @@
 package org.bitbucket.pshirshov.izumitk.test
 
+import java.net.{InetSocketAddress, Socket, URL}
+
 import com.google.inject.Injector
 import com.typesafe.scalalogging.StrictLogging
 import org.scalatest.exceptions.TestPendingException
+import resource.managed
+
+import scala.util.{Failure, Success, Try}
 
 /**
   */
+@ExposedTestScope
 trait ResourceVerifier {
   def verifyResource(injector: Injector): Unit
 
   def resourceUnavailable(e: Throwable): Boolean
+}
+
+@ExposedTestScope
+trait ServiceAvailabilityChecker {
+  this: TestResourceAvailabilityChecker =>
+
+  def verifyServiceAvailability(uri: String, timeout: Int): Unit = {
+    verifyServiceAvailability(new URL(uri), timeout)
+  }
+
+  def verifyServiceAvailability(uri: URL, timeout: Int): Unit = {
+    verifyServiceAvailability(uri.getHost, uri.getPort, timeout)
+  }
+
+  def verifyServiceAvailability(host: String, port: Int, timeout: Int): Unit = {
+    Try {
+      managed(new Socket()).acquireAndGet {
+        s =>
+          s.connect(new InetSocketAddress(host, port), timeout)
+      }
+    } match {
+      case Failure(f) =>
+        logger.info(s"Test skipped because of unavailable service: $host:$port, $f")
+        throw new TestPendingException
+      case Success(_) =>
+    }
+  }
 }
 
 @ExposedTestScope
