fbab64aad35dd8d82afbf4967643c857dd4d302a
Pavel Shirshov <pavel.shirshov@teckro.com>
2016-12-06 11:24:43 +0000

    minor test tools

--- lib/akka-http-hal/src/main/scala/org/bitbucket/pshirshov/izumitk/http/hal/modules/HalRestModule.scala
+++ lib/akka-http-hal/src/main/scala/org/bitbucket/pshirshov/izumitk/http/hal/modules/HalRestModule.scala
@@ -5,6 +5,7 @@ import net.codingwell.scalaguice.ScalaMultibinder
 import org.bitbucket.pshirshov.izumitk.HealthChecker
 import org.bitbucket.pshirshov.izumitk.akka.http.modules.AbstractRestModule
 import org.bitbucket.pshirshov.izumitk.akka.http.util._
+import org.bitbucket.pshirshov.izumitk.akka.http.util.cors.{CORS, DefaultCORS}
 import org.bitbucket.pshirshov.izumitk.akka.http.util.serialization.{PermissiveJacksonProtocol, SerializationProtocol}
 import org.bitbucket.pshirshov.izumitk.http.hal.{DefaultHalApiPolicy, HalApiPolicy}
 
