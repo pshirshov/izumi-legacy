5ed4d64f7eb3a44e4ce7556b78d6994fe25cfe86
Pavel Shirshov <pavel.shirshov@teckro.com>
2017-01-27 11:20:22 +0000

    c* facade module included into c* plugin

--- lib/cassandra/src/main/scala/org/bitbucket/pshirshov/izumitk/plugins/CassandraPlugin.scala
+++ lib/cassandra/src/main/scala/org/bitbucket/pshirshov/izumitk/plugins/CassandraPlugin.scala
@@ -1,13 +1,14 @@
 package org.bitbucket.pshirshov.izumitk.plugins
 
 import net.codingwell.scalaguice.ScalaModule
-import org.bitbucket.pshirshov.izumitk.cassandra.modules.CassandraModule
+import org.bitbucket.pshirshov.izumitk.cassandra.modules.{CassandraFacadeConfigModule, CassandraModule}
 import org.bitbucket.pshirshov.izumitk.cdi.Plugin
 
 /**
   */
 class CassandraPlugin extends Plugin {
   override def createPluginModules: Seq[ScalaModule] = Seq(
-    new CassandraModule
+    new CassandraModule()
+    , new CassandraFacadeConfigModule()
   )
 }
