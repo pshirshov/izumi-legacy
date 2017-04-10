e40bc66df2410598841d771f42a46b83aa288d98
Pavel Shirshov <pavel.shirshov@teckro.com>
2017-04-10 20:50:56 +0100

    different jwt key types supported

--- lib/akka-http/src/main/scala/org/bitbucket/pshirshov/izumitk/akka/http/auth/jwt/JwtSupport.scala
+++ lib/akka-http/src/main/scala/org/bitbucket/pshirshov/izumitk/akka/http/auth/jwt/JwtSupport.scala
@@ -1,49 +1,54 @@
 package org.bitbucket.pshirshov.izumitk.akka.http.auth.jwt
 
+import java.security.Key
+
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.ObjectNode
 import com.typesafe.scalalogging.StrictLogging
 import org.bitbucket.pshirshov.izumitk.json.JacksonMapper
-import org.jose4j.jwk.PublicJsonWebKey
-import org.jose4j.jws.{AlgorithmIdentifiers, JsonWebSignature}
+import org.jose4j.jws.JsonWebSignature
 import org.jose4j.jwt.JwtClaims
 import org.jose4j.jwt.consumer.JwtConsumerBuilder
 
 import scala.util.Try
 
-/**
-  */
+case class JwtKeyId(name: String)
+
+case class KeyPair(algorithmIdentifier: String, encryption: Option[Key], verification: Key)
+
+
 trait JwtSupport extends StrictLogging {
-  protected def getKey(keyId: String): PublicJsonWebKey
+  protected def getKey(keyId: JwtKeyId): KeyPair
 
   protected def jwtMapper: JacksonMapper
 
-  protected def createJwt(keyId: String, payload: String): String = {
+  protected def createJwt(keyId: JwtKeyId, payload: String): String = {
     val jws = new JsonWebSignature()
     jws.setPayload(payload)
-
-    val key = getKey(keyId)
-    jws.setKey(key.getPrivateKey)
-    jws.setKeyIdHeaderValue(key.getKeyId)
-    jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.RSA_USING_SHA256)
-
-    jws.getCompactSerialization
+    createJwt(keyId, jws)
   }
 
-  protected def createJwt(keyId: String, payload: JsonNode): String = {
+  protected def createJwt(keyId: JwtKeyId, payload: JsonNode): String = {
     val jws = new JsonWebSignature()
     jws.setPayload(jwtMapper.writeValueAsString(payload))
+    createJwt(keyId, jws)
+  }
 
+  protected def createJwt(keyId: JwtKeyId, jws: JsonWebSignature): String = {
     val key = getKey(keyId)
-    jws.setKey(key.getPrivateKey)
-    jws.setKeyIdHeaderValue(key.getKeyId)
-    jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.RSA_USING_SHA256)
-
+    key.encryption match {
+      case Some(e) =>
+        jws.setKey(e)
+      case None =>
+        throw new IllegalArgumentException(s"Encryption key not defined: $key")
+    }
+    jws.setKeyIdHeaderValue(keyId.name)
+    jws.setAlgorithmHeaderValue(key.algorithmIdentifier)
     jws.getCompactSerialization
   }
 
-  protected def readJwt(keyId: String, token: String): Try[JwtClaims] = {
-    val consumerBuilder = createConsumerBuilder.setVerificationKey(getKey(keyId).getKey)
+  protected def readJwt(keyId: JwtKeyId, token: String): Try[JwtClaims] = {
+    val consumerBuilder = createConsumerBuilder.setVerificationKey(getKey(keyId).verification)
     Try(consumerBuilder.build().processToClaims(token))
   }
 
