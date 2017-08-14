31a812a8f0651527e0d6fb92fa1fcd259306f436
Pavel Shirshov <pavel.shirshov@teckro.com>
2017-08-14 09:29:50 +0100

    jolokia made optional

--- lib/app-skeleton/src/main/scala/org/bitbucket/pshirshov/izumitk/app/modules/JmxMpModule.scala
+++ lib/app-skeleton/src/main/scala/org/bitbucket/pshirshov/izumitk/app/modules/JmxMpModule.scala
@@ -4,22 +4,16 @@ import javax.management.MBeanServer
 
 import com.google.inject.name.Named
 import com.google.inject.{Provides, Singleton}
-import net.codingwell.scalaguice.{ScalaModule, ScalaMultibinder}
+import net.codingwell.scalaguice.ScalaModule
 import org.bitbucket.pshirshov.izumitk.app.util.JMXUtils
 import org.bitbucket.pshirshov.izumitk.app.util.JMXUtils.JMXMPContext
-import org.jminix.console.servlet.MiniConsoleServlet
-import org.jolokia.http.AgentServlet
 
 
 /**
   */
 final class JmxMpModule() extends ScalaModule {
 
-  override def configure(): Unit = {
-    val servlets = ScalaMultibinder.newSetBinder[ServletBinding](binder)
-    servlets.addBinding.toInstance(ServletBinding("/console/*", classOf[MiniConsoleServlet]))
-    servlets.addBinding.toInstance(ServletBinding("/jolokia/*", classOf[AgentServlet]))
-  }
+  override def configure(): Unit = {}
 
   @Provides
   @Singleton
